f2f19f579f6430796e4613e9a66d3d189b896466
Ashim Rudra Paul <codewithashim@gmail.com>
2023-07-13 18:11:04 +0600

    if Else

---
+++ if_statement.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" = "Like" ] 
+then 
+ echo "Please Like My Code"
+else
+ echo "Oky Go The Hell"
+fi
--- shellScript.sh
+++ shellScript.sh
@@ -2,3 +2,28 @@
 
 echo "Hello Im Ashim Im a DevOps Engineer"
 
+#echo $BASH
+
+name="Ashim Rudra Paul"
+
+echo "Hello ${name} , Please Enter Your Age "
+
+read age
+
+echo "My Age Is ${age}"
+
+echo "hello "
+
+sleep 2
+
+echo "Hello how are you $1"
+
+sleep 2
+
+echo "Im Fine What About You Baby"
+
+sleep 2
+
+echo "im good too"
+
+
